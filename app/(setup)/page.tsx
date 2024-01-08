a2d65e292db99ad4e1b03a070ac0ba957eec4b2b
sarthakx30 <sarthakkharabanda@gmail.com>
2024-01-08 18:09:12 +0530

    uploadthing setup with create server

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -2,6 +2,8 @@ import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
 
+import { InitialModal } from "@/components/modals/initial-modal";
+
 const SetupPage= async ()=>{
     const profile=await initialProfile();
 
@@ -18,9 +20,7 @@ const SetupPage= async ()=>{
     if(server) return redirect(`/servers/${server.id}`);
 
     return(
-        <div>
-            Create a Server
-        </div>
+        <InitialModal/>
     )
 }
 
